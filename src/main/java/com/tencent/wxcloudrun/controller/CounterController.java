7238e983a97eeeba1d4c0a715ba531aef7097043
HaoHuan <aivny@hotmail.com>
2023-12-24 16:57:44 +0800

    init

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -1,21 +1,20 @@
 package com.tencent.wxcloudrun.controller;
 
-import com.tencent.wxcloudrun.AreasRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.tencent.wxcloudrun.*;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
 import com.tencent.wxcloudrun.service.CounterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-
+import com.alibaba.fastjson2.JSONObject;
 /**
  * counter控制器
  */
@@ -24,7 +23,15 @@ import java.util.Optional;
 public class CounterController {
 
   @Autowired
+  BrandsRepository brandsRepository;
+  @Autowired
+  TypesRepository typesRepository;
+  @Autowired
   AreasRepository areasRepository;
+  @Autowired
+  UsersRepository usersRepository;
+  @Autowired
+  DetailsRepository detailsRepository;
 
   final CounterService counterService;
   final Logger logger;
@@ -56,9 +63,103 @@ public class CounterController {
     return ApiResponse.ok("In service.");
   }
 
-  @GetMapping(value = "/getArea")
-  ApiResponse getArea(){
-    return ApiResponse.ok(areasRepository.findAll());
+  @RequestMapping("getType")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Iterable<TypesEntity> getType(){
+
+//        Map<String, Iterable<TypesEntity>> map = new HashMap<>();
+//        map.put("data",typesRepository.findAll());
+
+    return typesRepository.findAll();
+
+  }
+
+  @RequestMapping("getBrand")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Iterable<BrandsEntity> getBrand(@RequestBody String type){
+
+//        Map<String, Iterable<BrandsEntity>> map = new HashMap<>();
+//        map.put("data",brandsRepository.findByType(type));
+
+    return brandsRepository.findByType(type);
+
+  }
+
+  @RequestMapping("getArea")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Map<String, Iterable<AreasEntity>> getArea(){
+
+    Map<String, Iterable<AreasEntity>> map = new HashMap<>();
+    map.put("data",areasRepository.findAll());
+
+    return map;
+
+  }
+
+  @PostMapping("query")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Iterable<DetailsEntity> query(@RequestBody String id){
+    return detailsRepository.findDetailsEntitiesByNoContains(id);
+  }
+
+  @PostMapping("delete")
+  @CrossOrigin("*")
+  @ResponseBody
+  public Object delete(@RequestBody String id){
+    try{
+      detailsRepository.deleteById(id);
+    } catch(Exception e){
+      return e.toString();
+    }
+    return "success";
+  }
+
+  @PostMapping("login")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Object login(@RequestBody String req){
+    JSONObject jsonObject = JSONObject.parse(req);
+    String username = jsonObject.getString("username");
+    String password = jsonObject.getString("password");
+
+    JSONObject res = new JSONObject();
+    if (usersRepository.existsByUsernameAndPassword(username,password)){
+      res.put("result","ok");
+      res.put("uniqId","qwer1234");
+      return res;
+    } else {
+      res.put("result","notok");
+      res.put("msg","登录失败");
+      return res;
+    }
+
+  }
+
+  @PostMapping("/submit")
+  @CrossOrigin(origins = "*")
+  @ResponseBody
+  public Object submit(@RequestBody String req){
+    JSONObject jsonObject = JSONObject.parse(req);
+    DetailsEntity detailsEntity = new DetailsEntity();
+
+    try{
+      detailsEntity.setNo(jsonObject.getString("no"));
+      detailsEntity.setBrand(jsonObject.getString("brand"));
+      detailsEntity.setType(jsonObject.getString("type"));
+      detailsEntity.setArea(jsonObject.getString("area"));
+      detailsEntity.setModel(jsonObject.getString("model"));
+      detailsEntity.setSeat(jsonObject.getString("seat"));
+
+      detailsRepository.save(detailsEntity);
+    } catch (Exception e){
+      return e.toString();
+    }
+
+    return "success";
   }
   /**
    * 更新计数，自增或者清零
